c4f2ab4c55f32736a163a5b57b0cdb1ea36cf51a
Ondassyn <oabdrakhmanov@nu.edu.kz>
2020-05-04 17:48:51 +0600

    adding id setting to abstract map impl. closes #16

--- petclinic-web/src/main/java/dos/petclinicweb/bootstrap/DataLoader.java
+++ petclinic-web/src/main/java/dos/petclinicweb/bootstrap/DataLoader.java
@@ -21,14 +21,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Micha");
         owner2.setLastName("Preston");
 
@@ -37,14 +35,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Robert");
         vet1.setLastName("Polanski");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Roberta");
         vet2.setLastName("Politano");
 
