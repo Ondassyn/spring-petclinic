8fd30455f596931e153b4e3d7a17db29676da628
Ondassyn <oabdrakhmanov@nu.edu.kz>
2020-05-05 15:25:59 +0600

    test

---
+++ petclinic-data/src/test/java/dos/petclinicdata/PetclinicDataApplicationTests.java
@@ -0,0 +1,13 @@
+package dos.petclinicdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ petclinic-web/src/test/java/dos/petclinicweb/PetclinicWebApplicationTests.java
@@ -0,0 +1,13 @@
+package dos.petclinicweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetclinicWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
